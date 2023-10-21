620bf8b8dcdb85f7e2a7c82f67f4f7dd6411d85c
Rebeca Martim <rm92141@estudante.fieb.edu.br>
2023-10-20 22:24:32 -0300

    Limpeza dos comentários

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -15,17 +15,19 @@ import br.com.belval.crud.repository.ProdutoRepository;
 @Controller
 public class ProdutoController {
 	
-//	private static List<Produto> lista = new ArrayList<>();
-//	private static int proxId = 1;
+
 	@Autowired
 	private ProdutoRepository repository;
 
+	
+	
 	@GetMapping("/produto/novo")
 	public String novo(Model model) {
 		model.addAttribute("produto", new Produto());
 		return "produto";
 	}
 
+	
 	
 	@GetMapping("/produto/{id}/edit")
 	public String editar(@PathVariable int id, Model model) {
@@ -42,15 +44,15 @@ public class ProdutoController {
 		return "produto";
 	}
 	
+	
+	
 	@PostMapping("/produto/novo")
 	public ModelAndView novo(Produto produto, RedirectAttributes redirectAttributes) {
 		ModelAndView modelAndView = new ModelAndView("redirect:/produto/list");
 		
 		if (produto.getId() == 0) {
-//			insert(produto);
 			redirectAttributes.addFlashAttribute("msg","Novo produto criado!");
 		} else {
-//			update(produto);
 			redirectAttributes.addFlashAttribute("msg","Produto atualizado!");
 		}
 		repository.save(produto);
@@ -58,25 +60,10 @@ public class ProdutoController {
 		return modelAndView;
 	}
 	
-//	private void insert(Produto produto) {
-//	produto.setId(proxId++);
-//	lista.add(produto);
-//}
-
-//private void update(Produto produto) {
-//	ListIterator<Produto> it = lista.listIterator();
-//	while(it.hasNext()) {
-//		Produto encontrado = it.next();
-//		if (encontrado.getId() == produto.getId()) {
-//			it.remove();
-//			it.add(produto);
-//		}
-//	}
-//}
+	
 	
 	@GetMapping("/produto/list")
 	public String list(Model model) {
-//		model.addAttribute("produtos", lista);
 		model.addAttribute("produtos", repository.findAll());
 		return "lista-produtos";
 	}
@@ -93,31 +80,11 @@ public class ProdutoController {
 		
 		return "produto-nao-encontrado";
 	}
-	
-//	private Produto buscarPorId(int id) {
-//	Produto encontrou = null;
-//	for(Produto p : lista) {
-//		if (p.getId() == id) {
-//			
-//			encontrou = p;
-//			break;
-//		}
-//	}
-//	return encontrou;
-//}
+
 
 	
 	@GetMapping("/produto/{id}/excluir")
 	public String excluir(@PathVariable int id, RedirectAttributes redirectAttributes) {
-//		ListIterator<Produto> it = lista.listIterator();
-//		while(it.hasNext()) {
-//			Produto encontrado = it.next();
-//			if (encontrado.getId() == id) {
-//				it.remove();
-//				redirectAttributes.addFlashAttribute("msg","Produto excluído!");
-//				break;
-//			}
-//		}
 		repository.deleteById(id); 
 		
 		redirectAttributes.addFlashAttribute("msg","Produto excluído!");
